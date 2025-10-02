5b497cfd45657f0e71a988ee10718db14fdc40bd
anton <jappinen.anton@protonmail.com>
2025-10-02 09:11:57 +0200

    interfaces for creating a bot

--- App.tsx
+++ App.tsx
@@ -1,23 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { askAi } from './Api';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createChatBot } from "./Api.tsx";
 
+async function testBot(): Promise<void> {
+  const bot1 = createChatBot(
+    {
+      id: "123",
+      name: "chad",
+      system_instruction: "you are a classic gigachad",
+    },
+  );
+  console.log("bot created: ", bot1.name);
+
+  const message: string = "hi chad how are you?";
+
+  console.log("user message:", message);
+
+  const replay = await bot1.sendMessage(message);
+
+  console.log("bot replay: ", replay);
+}
 export default function App() {
-
-	console.log(askAi("how are you?"))
-	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
+  testBot();
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
