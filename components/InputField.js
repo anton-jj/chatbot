30c8094ce409c6c417f378781619215aa0eca401
anton <jappinen.anton@protonmail.com>
2025-10-08 09:43:05 +0200

    input empty on send, and chatworking even comming from startscreen

--- components/InputField.js
+++ components/InputField.js
@@ -6,54 +6,66 @@ import { ChatHistoryProvider, useChatHistory } from "../hooks/useChatHistory";
 const { width } = Dimensions.get("window")
 
 export function InputField() {
-  const [chatContent, setChatContent] = useState('');
-  const { sendMessage } = useChatHistory();
+	const [chatContent, setChatContent] = useState('');
+	const { sendMessage } = useChatHistory();
+	const chatPlaceHolder = "Type to chat.."
 
-  const handleSend = async () => {
-    await sendMessage(chatContent)
-  }
+	const handleSend = async () => {
+		if (!chatContent.trim()) {
+			return
+		}
+		const chat = chatContent
+		setChatContent(chatPlaceHolder)
+		try {
+			await sendMessage(chat)
+		} catch (e) {
+			console.error('something went wrong sending to the bot')
+		}
+	}
 
-  return (
-    <View style={style.InputContainer}>
-      <TextInput multiline style={style.inputfield} value={chatContent} onChangeText={setChatContent} placeholder="Type to chat.."></TextInput>
+	return (
+		<View style={style.InputContainer}>
+			<TextInput multiline style={style.inputfield}
+				value={chatContent}
+				onChangeText={setChatContent}
+				placeholder={chatPlaceHolder}></TextInput>
 
-      {/* änra till att skicka till API sen*/}
-      <TouchableOpacity style={style.sendButton} onPress={handleSend}>
-        <Cat size={25} color={"pink"} />
-      </TouchableOpacity>
-    </View>
-  )
+			<TouchableOpacity style={style.sendButton} onPress={handleSend}>
+				<Cat size={25} color={"pink"} />
+			</TouchableOpacity>
+		</View>
+	)
 }
 
 {/** Lite fulstyling till att börja med, fixa 100% bredd etc, fick det inte att funka */ }
 
 const style = StyleSheet.create({
-  InputContainer: {
-    flexDirection: "row",
-    backgroundColor: "#C68A9E",
-    justifyContent: "space-between",
-    alignItems: "center",
-    position: "absolute",
-    elevation: 5,
-    width: width,
-    maxHeight: 120,
-    minHeight: "fit-content",
-    bottom: 0,
-    borderRadius: 24,
-  },
+	InputContainer: {
+		flexDirection: "row",
+		backgroundColor: "#C68A9E",
+		justifyContent: "space-between",
+		alignItems: "center",
+		position: "absolute",
+		elevation: 5,
+		width: width,
+		maxHeight: 120,
+		minHeight: "fit-content",
+		bottom: 0,
+		borderRadius: 24,
+	},
 
-  inputfield: {
-    width: width - 60,
-    fontSize: 20,
-    paddingLeft: 20,
-  },
+	inputfield: {
+		width: width - 60,
+		fontSize: 20,
+		paddingLeft: 20,
+	},
 
-  sendButton: {
-    backgroundColor: "#553671",
-    justifyContent: "center",
-    alignItems: "center",
-    width: 45,
-    height: 45,
-    borderRadius: 24,
-  }
+	sendButton: {
+		backgroundColor: "#553671",
+		justifyContent: "center",
+		alignItems: "center",
+		width: 45,
+		height: 45,
+		borderRadius: 24,
+	}
 })
